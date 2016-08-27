1b8e420759d2c47a392894ff8644448155771efb
Yi Pan <ypan1988@gmail.com>
2016-08-27 21:16:53 +0100

    add dbl_ptr.cc

--- 02/dbl_ptr.cc
+++ 02/dbl_ptr.cc
@@ -8,4 +8,19 @@ int main()
   int ival = 1024;
   int *pi = &ival;		// pi points to an int
   int **ppi = &pi;		// ppi points to pointer to an int
+  cout << "The value of ival\n"
+       << "direct value: " << ival << "\n"
+       << "indirect value: " << *pi << "\n"
+       << "doubly indirect value: " << **ppi
+       << endl;
+
+  int i = 2;
+  pi = &i;		        // pi points to an int
+  *pi = *pi * *pi;		// equivalent to i = i * i;
+  cout << "i = " << i << endl;
+
+  *pi *= *pi;			// equivalent to i *= i;
+  cout << "i = " << i << endl;
+
+  return 0;
 }
