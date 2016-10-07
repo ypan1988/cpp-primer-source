8526cbd01fb9c29be7eeeff1b0b635bcfc87c531
Yi Pan <ypan1988@gmail.com>
2016-10-07 16:22:32 +0100

    add word_echo.cc and getline.cc

---
+++ 03/word_echo.cc
@@ -0,0 +1,17 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+  string word;
+  
+  while(cin >> word)		// read until end-of-file
+    cout << word << endl;	// write each word followed by a new line
+
+  return 0;
+}
