96a4d13fafdc23a050bda4b1dea8cf1b175ee742
Yi Pan <ypan1988@gmail.com>
2016-12-07 14:45:08 +0000

    add wdebug.cc

--- 06/wdebug.cc
+++ 06/wdebug.cc
@@ -4,6 +4,17 @@
 #include <iostream>
 using std::cout; using std::cerr; using std::endl;
 
+#include <string>
+using std::string;
+
+#include <cstddef>
+using std::size_t;
+
+#include <cassert>
+// assert is a preprocessor macro and therefore not in std
+// hence we need to include cassert header, 
+// but no using declaration for assert 
+
 void print(const int ia[], size_t size)
 {
   #ifndef NDEBUG
@@ -20,5 +31,20 @@ int main()
   int arr[sz] = {1, 2, 3, 4, 5};
   print(arr, sz);
 
+  string word = "foo";
+  const string::size_type threshold = 5;
+  if (word.size() < threshold) {
+    cerr << "Error: " << __FILE__
+	 << " : in function " << __func__
+	 << " at line " << __LINE__ << endl
+	 << "       Compiled on " << __DATE__
+	 << " at " << __TIME__ << endl
+	 << "       Word read was \"" << word
+	 << "\":  Length too short" << endl;
+  }
+
+  word = "something longer than five chars";
+  assert(word.size() > threshold);
+  
   return 0;
 }
