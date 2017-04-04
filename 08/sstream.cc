3fbc81cd19f2c10cd08ed3a5d08c6558d61c9d36
Yi Pan <ypan1988@gmail.com>
2017-04-04 10:59:09 +0100

    add sstreams.cc

---
+++ 08/sstream.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+using std::cin; using std::cout; using std::cerr;
+
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+// members are public by default
+struct PersonInfo {
+  string name;
+  vector<string> phones;
+};
+
+// we'll see how to reformat phone numbers in Chapter 17
+// for now just return the string we're given
+string format(const string &s) { return s; }
+
+// we'll see how to validate phone numbers in Chapter 17
+// for now just return true
+bool valid(cosnt string &s) { return true; }
+
+vector<PersonInfo>
+getData(istream &is) 
+{
+  // will hold a line and word from input, respectively
+  string line, word;
+
+  // will hold all the records from the input
+  vector<PersonInfo> people;
+
+  // read the input a line at a time until end-of-file (or other error)
+  while (getline(is, line)) {
+    PersonInfo info;		// object to hold this record's data
+    istringstream record(line);	// bind record to the line we just read
+    record >> info.name;	// read the name
+    while (record >> word) 	// read the phone numbers
+      info.phones.push_back(word); // and store them
+    people.push_back(info);	   // append this record to people
+  }
+
+  return people;
+}
+
+ostream& process(ostream &os, vector<PersonInfo> people)
+{
+  for (const auto &entry : people) {  // for each entry in people
+    ostringstream formatted, badNums; // objects created on each loop
+    for (const auto &nums : entry.phones) { // for each number
+      if (!valid(nums)) {
+	badNums << " " << nums;	// string in badNums
+      } else {
+	// "writes" to formatted's string
+	formatted << " " << format(nums);
+      }
+    }
+    if (badNums.str().empty())	// there were no bad numbers
+      os << entry.names << " "	// print the name
+	 << formatted.str() << endl; // and reformatted numbers
+    else
+      cerr << "input error: " << entry.name
+	   << " invalid number(s) " << badNums.str() << endl;
+  }
+    
+    return os;
+  }
+
+    int main()
+    {
+    process(cout, getData(cin));
+  }
