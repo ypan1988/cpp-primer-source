1ba3f356c8d1114c721e588f23f5ca8dbd013508
Yi Pan <ypan1988@gmail.com>
2017-05-16 23:56:23 +0100

    update query

--- 12/TextQuery.cc
+++ 12/TextQuery.cc
@@ -1,5 +1,30 @@
 #include "TextQuery.h"
 
+#include <cctype>
+using std::ispunct; using std::tolower;
+
+#include <iostream>
+using std::cout; using std::endl;
+using std::ostream; 
+
+#include <fstream>
+using std::ifstream;
+
+#include <sstream>
+using std::istringstream;
+
+#include <vector>
+using std::vector;
+
+#include <string>
+using std::string;
+
+#include <set>
+using std::set;
+
+#include <memory>
+using std::shared_ptr;
+
 // read the input file and build the map of lines to line numbers
 TextQuery::TextQuery(ifstream &is): file(new vector<string>)
 {
@@ -10,6 +35,7 @@ TextQuery::TextQuery(ifstream &is): file(new vector<string>)
     istringstream line(text);   // separate the line into words
     string word;
     while (line >> word) {      // for each word in that line
+      word = cleanup_str(word);
       // if word isn't already in wm, subscripting adds a new entry
       auto &lines = wm[word];   // lines is a shared_ptr
       if (!lines)               // that pointer is null the first time we see word
@@ -17,4 +43,73 @@ TextQuery::TextQuery(ifstream &is): file(new vector<string>)
       lines->intert(n);                // insert this line number
     }
   }
+}
+
+// not covered in the book -- cleanup_str removes
+// punctuation and converts all text to lowercase so that
+// the queries operate in a case insensitive manner
+string TextQuery::cleanup_str(const string &word)
+{
+  string ret;
+  for (auto it = word.begin(); it != word.end(); ++it) {
+    if (!ispunct(*it))
+      ret += tolower(*it);
+  }
+  return ret;
+}
+
+QueryResult
+TextQuery::query(const string &sought) const
+{
+  // we'll return a pointer to this set if we don't find sought
+  static shared_ptr<set<line_no>> nodata(new set<line_no>);
+
+  // use find and not a subscript to avoid adding words to wm!
+  auto loc = wm.find(cleanup_str(sought));
+  if (loc == wm.end()) 
+    return QueryResult(sought, nodata, file); // not found
+  else 
+    return QueryResult(sought, loc->second, file);
+}
+
+ostream& print(ostream &os, const QueryResult &qr)
+{
+  // if the word was found, print the count and all occurrences
+  os << qr.sought << " occurs " << qr.lines->size() << " "
+     << (qr.lines->size() ? "time" : "times") << endl;
+
+  // print each line in which the word appeared
+  for (auto num : *qr.lines)	// for every element in the set
+    // don't confound the user with text lines starting at 0
+    os << "\t(line " << num + 1 << ") "
+       << *(qr.file->begin() + num) << endl;
+
+  return os;
+}
+
+// debugging routine, not covered in the book
+void TextQuery::display_map()
+{
+  auto iter = wm.cbegin(), iter_end = wm.cend();
+  
+  // for each word in the map
+  for ( ; iter != iter_end; ++iter) {
+    cout << "word: " << iter->first << " {";
+
+    // fetch location vector as a const reference to avoid copying it
+    auto text_locs = iter->second;
+    auto loc_iter = text_locs->cbegin(),
+      loc_iter_end = text_locs->cend();
+
+    // print all line numbers for this word
+    while (loc_iter != loc_iter_end) {
+      cout << *loc_iter;
+      
+      if (++loc_iter != loc_iter_end)
+	cout << ", ";
+    }
+    
+    cout << "}\n";		// end list of output this word
+  }
+  cout << endl;			// finished printing entire map
 }
--- 12/TextQuery.h
+++ 12/TextQuery.h
@@ -1,17 +1,29 @@
 #ifndef TEXTQUERY_H
 #define TEXTQUERY_H
 
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <memory>
+#include "QueryResult.h"
+
 class QueryResult;              // declaration needed for return type in the query function
 class TextQuery {
-  public:
-    using line_no = std::vector<std::string>::size_type;
-    TextQuery(std::ifstream&);
-    QueryResult query(const std::string&) const;
-  private:
-    std::shared_ptr<std::vector<std::string>> file; // input file
-    // map of each word to the set of the lines in which that word appears
-    std::map<std::string,
-        std::shared_ptr<std::set<line_no>>> wn;
+ public:
+  using line_no = std::vector<std::string>::size_type;
+  TextQuery(std::ifstream&);
+  QueryResult query(const std::string&) const;
+  void display_map();		// debugging aid: print the map
+ private:
+  std::shared_ptr<std::vector<std::string>> file; // input file
+  // map each word to the set of the lines in which that word appears
+  std::map<std::string,
+    std::shared_ptr<std::set<line_no>>> wn;
+
+  // canonicalizes text: removes punctuation and makes everything lower case
+  static std::string cleanup_str(const std::string&);
 };
 
 #endif
