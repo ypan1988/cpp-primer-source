8ccd7e0e8b511ab889ae2a20e8d120e00cd51ae8
Yi Pan <ypan1988@gmail.com>
2017-08-26 16:17:59 +0100

    Update Folder

--- 13/Folder.cc
+++ 13/Folder.cc
@@ -1,65 +1,177 @@
 #include "Folder.h"
 
+#include<iostream>
+using std::endl; using std::cerr;
+
 void swap(Message &lhs, Message &rhs)
 {
-  using std::swap;              // not strictly needed in this case, but good habit
-  // remove pointers to each Message from their (original) respective Folders
-  for (auto f: lhs.folders)
-    f->remMsg(&lhs);
-  for (auto f: rhs.folders)
-    f->remMsg(&rhs);
-  // swap the contents and Folder pointer sets
-  swap(lhs.folders, rhs.folders);    // use swap(set& set&)
-  swap(lhs.contents, rhs.contents);  // swap(string&, string&)
-  // add pointers to each Message to their (new) respective Folders
-  for (auto f: lhs.folders)
-    f->addMsg(&lhs);
-  for (auto f: rhs.folders)
-    f->addMsg(&rhs);
+    using std::swap;              // not strictly needed in this case, but good habit
+    // remove pointers to each Message from their (original) respective Folders
+    for (auto f: lhs.folders)
+        f->remMsg(&lhs);
+    for (auto f: rhs.folders)
+        f->remMsg(&rhs);
+    // swap the contents and Folder pointer sets
+    swap(lhs.folders, rhs.folders);    // use swap(set& set&)
+    swap(lhs.contents, rhs.contents);  // swap(string&, string&)
+    // add pointers to each Message to their (new) respective Folders
+    for (auto f: lhs.folders)
+        f->addMsg(&lhs);
+    for (auto f: rhs.folders)
+        f->addMsg(&rhs);
 }
 
 Message::Message(const Message &m):
-  contents(m.contents), folders(m.folders)
+    contents(m.contents), folders(m.folders)
 {
-  add_to_Folders(m);            // add this Message to the Folders that point to m
+    add_to_Folders(m);            // add this Message to the Folders that point to m
 }
 
 Message& Message::operator=(const Message &rhs)
 {
-  // handle self-assignment by removing pointer before inserting them
-  remove_from_Folders();        // update existing Folders
-  contents = rhs.contents;      // copy message contents from rhs
-  folders = rhs.folders;        // copy Folder pointers from rhs
-  add_to_Folders(rhs);          // add this Message to those Folders
-  return *this;
+    // handle self-assignment by removing pointer before inserting them
+    remove_from_Folders();        // update existing Folders
+    contents = rhs.contents;      // copy message contents from rhs
+    folders = rhs.folders;        // copy Folder pointers from rhs
+    add_to_Folders(rhs);          // add this Message to those Folders
+    return *this;
+}
+
+Message::Message(Message &&m):
+    contents(std::move(contents))
+{
+    move_Folders(&m);
+}
+
+Message& Message::operator=(Message &&rhs)
+{
+    if (this != &rhs) {  // direct check for self-assignment
+        remove_from_Folders();
+        contents = std::move(rhs.contents);  // move assignment
+        move_Folders(&rhs);  // reset the Folders to point to this Message
+    }
+    return *this;
 }
 
 Message::~Message()
 {
-  remove_from_Folders();
+    remove_from_Folders();
 }
 
 void Message::save(Folder &f)
 {
-  folders.insert(&f);           // add the given Folder to our list of Folders
-  f.addMsg(this);               // add this Message to f's set of Messages
+    folders.insert(&f);           // add the given Folder to our list of Folders
+    f.addMsg(this);               // add this Message to f's set of Messages
 }
 
 void Message::remove(Folder &f)
 {
-  folders.erase(&f);            // tale the given Folder out of our list of Folders
-  f.remMsg(this);               // remove this Message to f's set of Messages
+    folders.erase(&f);            // tale the given Folder out of our list of Folders
+    f.remMsg(this);               // remove this Message to f's set of Messages
+}
+
+void Message::debug_print()
+{
+    cerr << "Message:\t" << contents << endl;
+    cerr << "Appears in " << folders.size() << " Folders" << endl;
+    cerr << endl;
 }
 
 // add this Message to Folders that point to m
 void Message::add_to_Folders(const Message &m)
 {
-  for (auto f : m.folders)      // for each Folder that holds m
-    f->addMsg(this);            // add a pointer to this Message to that Folder
+    for (auto f : m.folders)      // for each Folder that holds m
+        f->addMsg(this);            // add a pointer to this Message to that Folder
 }
 
 void Message::remove_from_Folders()
 {
-  for (auto f : folders)        // for each pointer in folders
-    f->remMsg(this);            // remove this Message
+    for (auto f : folders)        // for each pointer in folders
+        f->remMsg(this);            // remove this Message
+}
+
+void Message::move_Folders(Message *m)
+{
+    folders = std::move(m->folders);  // uses set move assignment
+    for (auto f : folders) {  // for each Folder
+        f->remMsg(m);     // remove the old Message from the Folder
+        f->addMsg(this);  // add this Message to that Folder
+    }
+    m->folders.clear();   // ensure that destroying m is harmless
+}
+
+Folder::Folder(const Folder &f)
+    : msgs(f.msgs)
+{
+    add_to_Messages(f);
+}
+
+Folder& Folder::operator =(const Folder &rhs)
+{
+    remove_from_Messages();
+    msgs = rhs.msgs;
+    add_to_Messages(rhs);
+    return *this;
+}
+
+Folder::Folder(Folder &&f)
+{
+    move_Messages(&f);
+}
+
+Folder& Folder::operator =(Folder &&rhs)
+{
+    if (this != &rhs) {
+        remove_from_Messages();
+        move_Messages(&rhs);
+    }
+    return *this;
+}
+
+Folder::~Folder() { remove_from_Messages(); }
+
+void Folder::save(Message &m)
+{
+    msgs.insert(&m);
+    m.addFldr(this);
+}
+
+void Folder::remove(Message &m)
+{
+    msgs.erase(&m);
+    m.remFldr(this);
+}
+
+void Folder::debug_print()
+{
+    cerr << "Folder contains " << msgs.size() << " messages" << endl;
+    int ctr = 0;
+    for (auto m : msgs) {
+        cerr << "Message " << ++ctr << ":\t" << m->contents << endl;
+    }
+    cerr << endl;
+}
+
+void Folder::add_to_Messages(const Folder &f)
+{
+    for (auto m : f.msgs) {
+        m->addFldr(this);
+    }
+}
+
+void Folder::remove_from_Messages()
+{
+    for (auto m : msgs) {
+        m->remFldr(this);
+    }
+}
+
+void Folder::move_Messages(Folder *f)
+{
+    msgs = std::move(f->msgs);
+    for (auto m : msgs) {
+        m->remFldr(f);
+        m->addFldr(this);
+    }
+    f->msgs.clear();
 }
--- 13/Folder.h
+++ 13/Folder.h
@@ -7,68 +7,69 @@
 class Folder;
 
 class Message {
-  friend void swap(Message&, Message&);
-  friend class Folder;
- public:
-  // folders is implicitly initialized to the empty set
-  explicit Message(const std::string &str = ""): contents(str) { }
+    friend void swap(Message&, Message&);
+    friend class Folder;
+public:
+    // folders is implicitly initialized to the empty set
+    explicit Message(const std::string &str = ""): contents(str) { }
 
-  // copy control to manage pointers to this Message
-  Message(const Message&);             // copy constructor
-  Message& operator=(const Message&);  // copy assignment
-  Message(Message&&);                  // move constructor
-  Message& operator=(Message&&);       // move assignment
-  ~Message();                          // destructor
+    // copy control to manage pointers to this Message
+    Message(const Message&);             // copy constructor
+    Message& operator=(const Message&);  // copy assignment
+    Message(Message&&);                  // move constructor
+    Message& operator=(Message&&);       // move assignment
+    ~Message();                          // destructor
 
-  // add/remove this Message from the specified Folder's set of messages
-  void save(Folder&);
-  void remove(Folder&);
-  void debug_print();           // print contents and it's list of Folders,
-                                // printing each Folder as well
- private:
-  std::string contents;         // actual message text
-  std::set<Folder*> folders;    // Folders that have this Message
+    // add/remove this Message from the specified Folder's set of messages
+    void save(Folder&);
+    void remove(Folder&);
+    void debug_print();           // print contents and it's list of Folders,
+    // printing each Folder as well
+private:
+    std::string contents;         // actual message text
+    std::set<Folder*> folders;    // Folders that have this Message
 
-  // utility functions used by copy constructor, assignment, and destructor
+    // utility functions used by copy constructor, assignment, and destructor
 
-  // add this Message to the Folders that point to the parameter
-  void add_to_Folders(const Message&);
-  // remove this Message from every Folder in folders
-  void remove_from_Folders();
-  void move_Folders(Message*);
+    // add this Message to the Folders that point to the parameter
+    void add_to_Folders(const Message&);
+    // remove this Message from every Folder in folders
+    void remove_from_Folders();
+    // move the Folder pointers from m to this Message
+    void move_Folders(Message*);
 
-  // used by Folder class to add self to this Message's set of Folder's
-  void addFldr(Folder *f) { folders.insert(f); }
-  void remFldr(Folder *f) { folders.erase(f); }
+    // used by Folder class to add self to this Message's set of Folder's
+    void addFldr(Folder *f) { folders.insert(f); }
+    void remFldr(Folder *f) { folders.erase(f); }
 };
 // declaration for swap should be in the same header as Message itself
 void swap(Message&, Message&);
 
 class Folder {
-  friend void swap(Message&, Message&);
-  friend class Message;
- public:
-  Folder() = default;		    // defaults ok
-  Folder(const Folder&);	    // add new folder to each Message inmsgs
-  Folder& operator=(const Folder&); // delete Folder from lhs messages
-                                    // add Folder to rhs messages
-  Folder(Folder &&);		    // move Messages to this Folder
-  Folder& operator=(Folder&&);	    // delete Folder from lhs messages
-  ~Folder();			    // remove self from Messages in msgs
+    friend void swap(Message&, Message&);
+    friend class Message;
+public:
+    Folder() = default;		    // defaults ok
+    Folder(const Folder&);	    // add new folder to each Message inmsgs
+    Folder& operator=(const Folder&); // delete Folder from lhs messages
+    // add Folder to rhs messages
+    Folder(Folder &&);		    // move Messages to this Folder
+    Folder& operator=(Folder&&);	    // delete Folder from lhs messages
+    ~Folder();			    // remove self from Messages in msgs
 
-  void save(Message&);		// add this message to folder
-  void remove(Message&);	// remove this message from this folder
-  void debug_print();		// print contents and it's list of Folders
+    void save(Message&);		// add this message to folder
+    void remove(Message&);	// remove this message from this folder
+    void debug_print();		// print contents and it's list of Folders
 
- private:
-  std::set<Message*> msgs;	// messages in this folder
+private:
+    std::set<Message*> msgs;	// messages in this folder
 
-  void add_to_Messages(const Folder&); // add this Folder to each Message
-  void remove_from_Messages();	       // remove this Folder from each Message
-  void move_Messages(Folder*);         // move Message pointers to point to this Folder
-  
-  void addMsg(Message *m) { msgs.insert(m); }
-  void remMsg(Message *m) { msgs.erase(m); }
+    void add_to_Messages(const Folder&); // add this Folder to each Message
+    void remove_from_Messages();	       // remove this Folder from each Message
+    void move_Messages(Folder*);         // move Message pointers to point to this Folder
+
+    void addMsg(Message *m) { msgs.insert(m); }
+    void remMsg(Message *m) { msgs.erase(m); }
 };
 
 #endif
